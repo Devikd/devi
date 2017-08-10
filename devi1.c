41a8d481a987e1120638759550c92d25fdd5dd1e
Devikd <devikumar1998@yahoo.com>
2017-08-10 14:45:54 +0530

    Create devi1.c

---
+++ devi1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b;
+if(a%4==0)
+{
+printf("\n leap year");
+scanf("%d",&a);
+}
+else
+{
+printf("\n enter not a leap year");
+}
+return 0;
+}
+

b741e3c9f6dcf7930c57c67be244e2929865abd3
Devikd <devikumar1998@yahoo.com>
2018-09-03 14:28:53 +0530

    Rename 82.py to 82pgm.py

--- 82pgm.py
+++ 82pgm.py
@@ -1,4 +1,4 @@
-num=int(input("Enter the number of lines"))
-for i in range(num):
+number=int(input("Enter the number of lines"))
+for i in range(number):
     v,g=map(int,input("Ninja's and kabali's value").split(' '))
     print(abs(v-g))

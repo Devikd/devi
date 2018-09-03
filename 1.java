b041f8fc11a6e018cf8ba50b34960595449a138d
Devikd <devikumar1998@yahoo.com>
2018-09-03 15:32:16 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class Shaa
+{
+    public static void main(String[]args)
+    {
+        Scanner data=new Scanner(System.in);
+        char[] v =data.next().toCharArray();
+        char[] g =data.next().toCharArray();
+    for(int i=0;i<=a.length;i++)
+        {
+           if(v[i]==g[i])
+                System.out.print(v[i]);
+                
+                
+            
+        }
+    }
+}

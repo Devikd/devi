271f0e151f00f44ef46add12da0252c57f317049
Devikd <devikumar1998@yahoo.com>
2018-09-12 14:51:43 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,8 +1,8 @@
-def gcd(a,b):
-    while b > 0:
-        a, b = b, a % b
-    return a
-def lcm(a, b):
-    return a * b / gcd(a, b)
-a,b=map(int,input("Enter the values:").split(' '))
-print(int(lcm(a,b)))
+def gcd(aa,ab):
+    while ab > 0:
+        aa, ab = ab, aa % ab
+    return aa
+def lcm(aa, ab):
+    return aa * ab / gcd(aa, ab)
+aa,ab=map(int,input("Enter the values:").split(' '))
+print(int(lcm(aa,ab)))

dfe009287acf7f714046b4f4b696d5b90a251001
Devikd <devikumar1998@yahoo.com>
2018-09-12 14:58:40 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+        char ss[10];
+        int ii;
+        scanf("%s",&s[ii]);
+        int l=strlen(ss);
+        for(ii=0;ii<l;ii++)
+        {
+                ss[ii]=ss[ii]+3;
+                printf("%c",ss[ii]);
+        }
+}

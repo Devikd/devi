102b0696a56facb34b5d728b18348f30c27629f0
Devikd <devikumar1998@yahoo.com>
2019-01-02 12:17:43 +0530

    Create 6prgrm.py

---
+++ 6prgrm.py
@@ -0,0 +1,8 @@
+Input:  str1 = "bbb", str2 = "xxy"
+Output: True
+'b' is mapped to 'x' and 'b' is mapped to 'y'.
+
+Input:  str1 = "bbb", str2 = "xyz"
+Output: False
+One occurrence of 'a' in str1 has 'x' in str2 and 
+other occurrence of 'b' has 'y'.
